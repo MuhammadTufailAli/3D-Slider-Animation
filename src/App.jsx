5d3ee1af9195b4d2c28245551d39643d707bbc31
Tufail Ali <tufailali@Tufails-MacBook-Air.local>
2024-06-03 16:59:34 +0500

    Slider animation using css

---
+++ src/App.jsx
@@ -0,0 +1,93 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='outer-container'>
+      
+    <div class="main-rail-wrapper">
+    <div class="inner-shadow"></div>
+    <div class="cc-rail-flip">
+
+
+     {/* First Column */}
+       <div data-w-id="78f85ea1-8be1-1b94-2210-9cd5fbb5f160" class="col-rail">
+        {/* Every column have 2 identical div to create infinte animation */}
+          <div class="inner-col-rail1" >
+             <div class="card-logo-image"><img src="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65e9f1d5c03bfc02bb103357_Philips_logo_new%202.svg" loading="lazy" alt="" class="cc-logo-image"/></div>
+             <div class="card-startup-image"><img src="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65cabca1db72d456cf6fbd4f_card-kajabi.png" loading="lazy" sizes="(max-width: 479px) 55vw, (max-width: 767px) 98vw, (max-width: 991px) 65vw, 26vw" srcset="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65cabca1db72d456cf6fbd4f_card-kajabi-p-500.png 500w, https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65cabca1db72d456cf6fbd4f_card-kajabi.png 626w" alt="" class="card-image-rail"/></div>
+             <div class="card-logo-image"><img src="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65e9f250e137bdb6b3d91886_petal.svg" loading="lazy" alt="" class="cc-logo-image"/></div>
+             <div class="card-startup-image"><img src="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65cabca1db72d456cf6fbcd7_card-dropbox.webp" loading="lazy" sizes="(max-width: 991px) 100vw, 39vw" srcset="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65cabca1db72d456cf6fbcd7_card-dropbox-p-500.webp 500w, https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65cabca1db72d456cf6fbcd7_card-dropbox.webp 626w" alt="" class="card-image-rail"/></div>
+          </div>
+          <div class="inner-col-rail1" >
+             <div class="card-logo-image"><img src="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65e9f1d5c03bfc02bb103357_Philips_logo_new%202.svg" loading="lazy" alt="" class="cc-logo-image"/></div>
+             <div class="card-startup-image"><img src="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65cabca1db72d456cf6fbd4f_card-kajabi.png" loading="lazy" sizes="(max-width: 479px) 55vw, (max-width: 767px) 98vw, (max-width: 991px) 65vw, 26vw" srcset="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65cabca1db72d456cf6fbd4f_card-kajabi-p-500.png 500w, https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65cabca1db72d456cf6fbd4f_card-kajabi.png 626w" alt="" class="card-image-rail"/></div>
+             <div class="card-logo-image"><img src="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65e9f250e137bdb6b3d91886_petal.svg" loading="lazy" alt="" class="cc-logo-image"/></div>
+             <div class="card-startup-image"><img src="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65cabca1db72d456cf6fbcd7_card-dropbox.webp" loading="lazy" sizes="(max-width: 991px) 100vw, 39vw" srcset="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65cabca1db72d456cf6fbcd7_card-dropbox-p-500.webp 500w, https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65cabca1db72d456cf6fbcd7_card-dropbox.webp 626w" alt="" class="card-image-rail"/></div>
+          </div>
+       </div>
+
+       {/* Second Column */}
+       <div data-w-id="78f85ea1-8be1-1b94-2210-9cd5fbb5f173" class="col-rail">
+          <div class="inner-col-rail" >
+             <div class="card-startup-image"><img src="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65e9f3fe327e773227f92742_image%201335.png" loading="lazy" sizes="(max-width: 991px) 100vw, 36vw" srcset="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65e9f3fe327e773227f92742_image%201335-p-500.png 500w, https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65e9f3fe327e773227f92742_image%201335-p-800.png 800w, https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65e9f3fe327e773227f92742_image%201335-p-1080.png 1080w, https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65e9f3fe327e773227f92742_image%201335.png 1201w" alt="" class="card-image-rail"/></div>
+             <div class="card-logo-image"><img src="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65ea6ee6efe9778b6d19a396_station.svg" loading="lazy" alt="" class="cc-logo-image"/></div>
+             <div class="card-startup-image"><img src="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65cabca1db72d456cf6fbcd3_card-getaround-2.webp" loading="lazy" sizes="(max-width: 991px) 100vw, 65vw" srcset="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65cabca1db72d456cf6fbcd3_card-getaround-2-p-500.webp 500w, https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65cabca1db72d456cf6fbcd3_card-getaround-2.webp 626w" alt="" class="card-image-rail"/></div>
+             <div class="card-logo-image"><img src="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65e9f1d51b2871d7ddb51416_Dropbox_logo_2017%202.svg" loading="lazy" alt="" class="cc-logo-image"/></div>
+          </div>
+          <div class="inner-col-rail" >
+             <div class="card-startup-image"><img src="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65e9f3fe327e773227f92742_image%201335.png" loading="lazy" sizes="(max-width: 991px) 100vw, 36vw" srcset="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65e9f3fe327e773227f92742_image%201335-p-500.png 500w, https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65e9f3fe327e773227f92742_image%201335-p-800.png 800w, https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65e9f3fe327e773227f92742_image%201335-p-1080.png 1080w, https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65e9f3fe327e773227f92742_image%201335.png 1201w" alt="" class="card-image-rail"/></div>
+             <div class="card-logo-image"><img src="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65ea6ee6efe9778b6d19a396_station.svg" loading="lazy" alt="" class="cc-logo-image"/></div>
+             <div class="card-startup-image"><img src="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65cabca1db72d456cf6fbcd3_card-getaround-2.webp" loading="lazy" sizes="(max-width: 991px) 100vw, 65vw" srcset="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65cabca1db72d456cf6fbcd3_card-getaround-2-p-500.webp 500w, https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65cabca1db72d456cf6fbcd3_card-getaround-2.webp 626w" alt="" class="card-image-rail"/></div>
+             <div class="card-logo-image"><img src="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65e9f1d51b2871d7ddb51416_Dropbox_logo_2017%202.svg" loading="lazy" alt="" class="cc-logo-image"/></div>
+          </div>
+          <div class="inner-col-rail" >
+             <div class="card-startup-image"><img src="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65e9f3fe327e773227f92742_image%201335.png" loading="lazy" sizes="(max-width: 991px) 100vw, 36vw" srcset="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65e9f3fe327e773227f92742_image%201335-p-500.png 500w, https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65e9f3fe327e773227f92742_image%201335-p-800.png 800w, https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65e9f3fe327e773227f92742_image%201335-p-1080.png 1080w, https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65e9f3fe327e773227f92742_image%201335.png 1201w" alt="" class="card-image-rail"/></div>
+             <div class="card-logo-image"><img src="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65ea6ee6efe9778b6d19a396_station.svg" loading="lazy" alt="" class="cc-logo-image"/></div>
+             <div class="card-startup-image"><img src="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65cabca1db72d456cf6fbcd3_card-getaround-2.webp" loading="lazy" sizes="(max-width: 991px) 100vw, 65vw" srcset="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65cabca1db72d456cf6fbcd3_card-getaround-2-p-500.webp 500w, https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65cabca1db72d456cf6fbcd3_card-getaround-2.webp 626w" alt="" class="card-image-rail"/></div>
+             <div class="card-logo-image"><img src="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65e9f1d51b2871d7ddb51416_Dropbox_logo_2017%202.svg" loading="lazy" alt="" class="cc-logo-image"/></div>
+          </div>
+       </div>
+
+       {/* Third Column */}
+       <div data-w-id="78f85ea1-8be1-1b94-2210-9cd5fbb5f186" class="col-rail">
+          <div class="inner-col-rail" >
+             <div class="card-logo-image"><img src="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65ea6ee6efe9778b6d19a396_station.svg" loading="lazy" alt="" class="cc-logo-image"/></div>
+             <div class="card-startup-image"><img src="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65e9f46e2603dc33e5dea7b1_6388cd67a3e39927ca4d40b1_mural-brand-card-wordmark-2.png" loading="lazy" sizes="(max-width: 479px) 87vw, (max-width: 767px) 100vw, (max-width: 991px) 92vw, 32vw" srcset="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65e9f46e2603dc33e5dea7b1_6388cd67a3e39927ca4d40b1_mural-brand-card-wordmark-2-p-500.png 500w, https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65e9f46e2603dc33e5dea7b1_6388cd67a3e39927ca4d40b1_mural-brand-card-wordmark-2-p-800.png 800w, https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65e9f46e2603dc33e5dea7b1_6388cd67a3e39927ca4d40b1_mural-brand-card-wordmark-2-p-1080.png 1080w, https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65e9f46e2603dc33e5dea7b1_6388cd67a3e39927ca4d40b1_mural-brand-card-wordmark-2.png 1600w" alt="" class="card-image-rail"/></div>
+             <div class="card-logo-image"><img src="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65ea6ee0793c27bb8e703409_ycombinator.svg" loading="lazy" alt="" class="cc-logo-image"/></div>
+             <div class="card-startup-image"><img src="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65ea7314003d4ca98addcb62_Frame%2024.png" loading="lazy" sizes="(max-width: 991px) 100vw, 52vw" srcset="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65ea7314003d4ca98addcb62_Frame%2024-p-500.png 500w, https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65ea7314003d4ca98addcb62_Frame%2024.png 626w" alt="" class="card-image-rail"/></div>
+             <div class="card-logo-image"><img src="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65ea6ef432f0a018a70a8fb0_vc-logos.svg" loading="lazy" alt="" class="cc-logo-image"/></div>
+             <div class="card-startup-image"><img src="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65ea73140244d4001eda7933_Frame%2029.png" loading="lazy" sizes="100vw" srcset="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65ea73140244d4001eda7933_Frame%2029-p-500.png 500w, https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65ea73140244d4001eda7933_Frame%2029.png 626w" alt="" class="card-image-rail"/></div>
+          </div>
+          <div class="inner-col-rail" >
+             <div class="card-logo-image"><img src="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65ea6ee6efe9778b6d19a396_station.svg" loading="lazy" alt="" class="cc-logo-image"/></div>
+             <div class="card-startup-image"><img src="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65e9f46e2603dc33e5dea7b1_6388cd67a3e39927ca4d40b1_mural-brand-card-wordmark-2.png" loading="lazy" sizes="(max-width: 479px) 87vw, (max-width: 767px) 100vw, (max-width: 991px) 92vw, 32vw" srcset="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65e9f46e2603dc33e5dea7b1_6388cd67a3e39927ca4d40b1_mural-brand-card-wordmark-2-p-500.png 500w, https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65e9f46e2603dc33e5dea7b1_6388cd67a3e39927ca4d40b1_mural-brand-card-wordmark-2-p-800.png 800w, https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65e9f46e2603dc33e5dea7b1_6388cd67a3e39927ca4d40b1_mural-brand-card-wordmark-2-p-1080.png 1080w, https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65e9f46e2603dc33e5dea7b1_6388cd67a3e39927ca4d40b1_mural-brand-card-wordmark-2.png 1600w" alt="" class="card-image-rail"/></div>
+             <div class="card-logo-image"><img src="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65ea6ee0793c27bb8e703409_ycombinator.svg" loading="lazy" alt="" class="cc-logo-image"/></div>
+             <div class="card-startup-image"><img src="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65ea7314003d4ca98addcb62_Frame%2024.png" loading="lazy" sizes="(max-width: 991px) 100vw, 52vw" srcset="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65ea7314003d4ca98addcb62_Frame%2024-p-500.png 500w, https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65ea7314003d4ca98addcb62_Frame%2024.png 626w" alt="" class="card-image-rail"/></div>
+             <div class="card-logo-image"><img src="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65ea6ef432f0a018a70a8fb0_vc-logos.svg" loading="lazy" alt="" class="cc-logo-image"/></div>
+             <div class="card-startup-image"><img src="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65ea73140244d4001eda7933_Frame%2029.png" loading="lazy" sizes="100vw" srcset="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65ea73140244d4001eda7933_Frame%2029-p-500.png 500w, https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65ea73140244d4001eda7933_Frame%2029.png 626w" alt="" class="card-image-rail"/></div>
+          </div>
+       </div>
+
+       {/* Fourth Column */}
+       <div data-w-id="78f85ea1-8be1-1b94-2210-9cd5fbb5f1a1" class="col-rail">
+          <div class="inner-col-rail" >
+             <div class="card-logo-image"><img src="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65e9f1d5c03bfc02bb103357_Philips_logo_new%202.svg" loading="lazy" alt="" class="cc-logo-image"/></div>
+             <div class="card-logo-image"><img src="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65cabca1db72d456cf6fbccf_Attentive.svg" loading="lazy" alt="" class="cc-logo-image"/></div>
+             <div class="card-logo-image"><img src="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65cabca1db72d456cf6fbd43_pacific-funds.svg" loading="lazy" alt="" class="cc-logo-image"/></div>
+             <div class="card-logo-image"><img src="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65cabca1db72d456cf6fbccc_logo-rakuten%201.svg" loading="lazy" alt="" class="cc-logo-image"/></div>
+          </div>
+          <div class="inner-col-rail" >
+             <div class="card-logo-image"><img src="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65e9f1d5c03bfc02bb103357_Philips_logo_new%202.svg" loading="lazy" alt="" class="cc-logo-image"/></div>
+             <div class="card-logo-image"><img src="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65cabca1db72d456cf6fbccf_Attentive.svg" loading="lazy" alt="" class="cc-logo-image"/></div>
+             <div class="card-logo-image"><img src="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65cabca1db72d456cf6fbd43_pacific-funds.svg" loading="lazy" alt="" class="cc-logo-image"/></div>
+             <div class="card-logo-image"><img src="https://cdn.prod.website-files.com/65cabca1db72d456cf6fbc46/65cabca1db72d456cf6fbccc_logo-rakuten%201.svg" loading="lazy" alt="" class="cc-logo-image"/></div>
+          </div>
+       </div>
+    </div>
+ </div>
+ </div>
+  );
+}
+
+export default App;
